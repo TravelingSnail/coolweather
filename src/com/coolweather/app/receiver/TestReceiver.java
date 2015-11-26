e383425921087754331f075e117c8276759cba5e
villagecanteen <123456789@qq.com>
2015-11-27 00:16:37 +0800

    创建活动包、广播接收器包、服务包.

---
+++ src/com/coolweather/app/receiver/TestReceiver.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.coolweather.app.receiver;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ *
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * @author 大师兄
+ * @ClassName TestReceiver
+ * @Version v1.0
+ * @ModifiedBy 大师兄
+ * @Copyright 农村小卖部
+ * @date 2015年11月27日 上午12:15:59
+ */
+public class TestReceiver {
+
+}
